2464fa2a976c65f7539aeb6a244abffcfeb26aeb
Jason Francis <jkfranci@gmail.com>
2020-08-30 22:57:08 -0400

    Initial DB migration.

---
+++ Migrations/20200831025441_init-database.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mangaQuotes.Migrations
+{
+    public partial class initdatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Characters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Characters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Quotes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ParentQuoteId = table.Column<int>(nullable: true),
+                    CharacterId = table.Column<int>(nullable: false),
+                    Chapter = table.Column<string>(nullable: false),
+                    Page = table.Column<int>(nullable: false),
+                    Text = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quotes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Quotes_Characters_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "Characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Quotes_Quotes_ParentQuoteId",
+                        column: x => x.ParentQuoteId,
+                        principalTable: "Quotes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quotes_CharacterId",
+                table: "Quotes",
+                column: "CharacterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quotes_ParentQuoteId",
+                table: "Quotes",
+                column: "ParentQuoteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quotes");
+
+            migrationBuilder.DropTable(
+                name: "Characters");
+        }
+    }
+}
